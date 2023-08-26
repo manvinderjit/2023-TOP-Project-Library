701e9ecf2fa5008545ae201d28fb65951a54aafb
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-08-25 23:57:31 -0400

    feat: Add booklist generation in HTML

--- script.js
+++ script.js
@@ -1,34 +1,59 @@
 // Array to store all book objects
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    
+const bookListContainer = document.getElementById("book-list");
+
+function generateBookSerialNo() {
+    if(myLibrary.length == 0) {
+        return 1;
+    }
+    return myLibrary.length + 1;
+}
+
+function Book(title, author, pages, read) {    
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 
-    this.bookInfo = function() {
-        return `${this.title} by ${this.author}, ${this.pages}, ${this.read ? 'has been read' : 'not read yet'}.`;
-    }
+    // this.bookInfo = function() {
+    //     return `${this.title} by ${this.author}, ${this.pages}, ${this.read ? 'has been read' : 'not read yet'}.`;
+    // }
 }
 
 const hobbitBook = new Book ('The Hobbit', 'J.R.R. Tolkein', 295, false);
+addBookToLibrary(hobbitBook);
 const hpBook = new Book ('Harry Potter', 'J.K. Rowling', 500, false);
+addBookToLibrary(hpBook);
 
 
-console.log(hobbitBook.bookInfo());
+// console.log(hobbitBook.bookInfo());
 
-function addBookToLibrary(book) {
+function addBookToLibrary(book) {    
     myLibrary.push(book);
 }
 
-function displayAllBooksInLibrary() {
-    myLibrary.forEach((book) => console.log(book));
+function createBookRow(book) {
+    let bookRow = document.createElement('div');
+    bookRow.classList.add("book-row");    
+    // let fieldSerialNo = document.createElement('div');
+    // fieldSerialNo.textContent = generateBookSerialNo();
+    // bookRow.appendChild(fieldSerialNo);
+
+    for(const property in book) {        
+        let fieldDiv = document.createElement('div');
+        fieldDiv.textContent = book[property];
+        bookRow.appendChild(fieldDiv);
+    }
+    return bookRow;
 }
 
-addBookToLibrary(hobbitBook);
-addBookToLibrary(hpBook);
+function displayAllBooksInLibrary() {
+    myLibrary.forEach((book) => {
+        bookListContainer.appendChild(createBookRow(book));
+    });
+}
+
 
 displayAllBooksInLibrary();
 // console.log(myLibrary);
