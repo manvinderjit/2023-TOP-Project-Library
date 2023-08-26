c854bb19ddff95a644f83aecee2c9d9d56c844fe
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-08-26 11:46:04 -0400

    fix: Fix book serial no generation

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // Array to store all book objects
 const myLibrary = [];
-
 const bookListContainer = document.getElementById("book-list");
 
+let nextBookSerialNo;
+
+function generateBookSerialNo() {
+    return (myLibrary.length == 0) ? 1: (myLibrary[myLibrary.length-1].serialNo + 1);
+}
+
 function Book(title, author, pages, read) {    
+    this.serialNo = generateBookSerialNo();
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -14,16 +20,6 @@ function Book(title, author, pages, read) {
     // }
 }
 
-
-
-const hobbitBook = new Book ('The Hobbit', 'J.R.R. Tolkein', 295, false);
-addBookToLibrary(hobbitBook);
-const hpBook = new Book ('Harry Potter', 'J.K. Rowling', 500, false);
-addBookToLibrary(hpBook);
-
-
-// console.log(hobbitBook.bookInfo());
-
 function addBookToLibrary(book) {    
     myLibrary.push(book);
 }
@@ -33,13 +29,9 @@ function createBookRow(book, bookIndex) {
     let bookRow = document.createElement('div');
     bookRow.classList.add("book-row");    
 
-    let fieldSerialNo = document.createElement('div');
-    fieldSerialNo.textContent = bookIndex + 1;
-    bookRow.appendChild(fieldSerialNo);
-
     for(const property in book) {        
         let fieldDiv = document.createElement('div');
-        fieldDiv.textContent = book[property];
+        fieldDiv.textContent = book[property];        
         bookRow.appendChild(fieldDiv);
     }
     return bookRow;
@@ -52,4 +44,11 @@ function displayAllBooksInLibrary() {
     });
 }
 
+const hobbitBook = new Book ('The Hobbit', 'J.R.R. Tolkein', 295, false);
+addBookToLibrary(hobbitBook);
+const hpBook = new Book ('Harry Potter', 'J.K. Rowling', 500, false);
+addBookToLibrary(hpBook);
+
 displayAllBooksInLibrary();
+
+console.log(myLibrary);
